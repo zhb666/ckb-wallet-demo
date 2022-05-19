ad55f66bc22bcbad9561ba1bb9d1195481276ab1
zhb666 <462612421@qq.com>
2022-05-19 16:28:21 +0800

    feat: jest

--- src/containers/RpcPage/index.tsx
+++ src/containers/RpcPage/index.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { A } from "hookrouter";
-import ReactJson from 'react-json-view'
-import { setScripts, getScripts, get_cells, get_transactions, get_cells_capacity } from '../../rpc';
-import { getCapacity } from "../../utils"
-import { div } from "../../utils/bigNumber"
+import ReactJson from "react-json-view";
+import {
+	setScripts,
+	getScripts,
+	get_cells,
+	get_transactions,
+	get_cells_capacity
+} from "../../rpc";
+import { getCapacity } from "../../utils";
+import { div } from "../../utils/bigNumber";
 
 import "./index.scss";
-
 
 const Component: React.FC = () => {
 	const [balance, setBalance] = useState<any>(0);
@@ -14,34 +19,40 @@ const Component: React.FC = () => {
 	const [transactions, setTransactions] = useState<any>({});
 
 	async function getBalance() {
-		let capacity = await get_cells_capacity()
-		let balance = div(await getCapacity(capacity), 8)
-		setBalance(balance.toString())
+		let capacity = await get_cells_capacity();
+		let balance = div(await getCapacity(capacity), 8);
+		setBalance(balance.toString());
 	}
 
 	async function getCells() {
-		let cells = await get_cells()
-		setCells(cells)
+		let cells = await get_cells();
+		setCells(cells);
 	}
 
 	async function getTransactions() {
-		let transactions = await get_transactions()
-		setTransactions(transactions)
+		let transactions = await get_transactions();
+		setTransactions(transactions);
 	}
 
 	return (
-		<main className="home">
-			<p><button onClick={setScripts}>setScripts</button></p>
-			<p><button onClick={getScripts}>getScripts</button></p>
-			<p>
+		<main className="rpc">
+			<div>
+				<button onClick={setScripts}>setScripts</button>
+			</div>
+			<div>
+				<button onClick={getScripts}>getScripts</button>
+			</div>
+			<div>
 				<button onClick={getCells}>get_cells </button>
 				<ReactJson theme={"tomorrow"} src={cells} />
-			</p>
-			<p>
+			</div>
+			<div>
 				<button onClick={getTransactions}>get_transactions</button>
 				<ReactJson theme={"tomorrow"} src={transactions} />
-			</p>
-			<p><button onClick={getBalance}>get_cells_capacity</button> : {balance} ckb</p>
+			</div>
+			<div>
+				<button onClick={getBalance}>get_cells_capacity</button> : {balance} ckb
+			</div>
 		</main>
 	);
 };
